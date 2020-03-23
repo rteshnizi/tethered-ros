d21162f5d3c3d100dea11f56c1b8398472b3fe1e
Madeleine <mphillips848676@tamu.edu>
2020-03-23 11:19:00 -0500

    fixed camera2 and organized

--- camera2.py
+++ camera2.py
@@ -12,7 +12,7 @@ class OutputForRoute:
         self.obstacles.append(arr)
         
 output = OutputForRoute()
-img = cv2.imread("dots.png")
+img = cv2.imread("photos/boarders.png")
 #img = cv2.resize(img1, (0,0), fx=0.15, fy=0.15) 
 original = img.copy()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -67,21 +67,36 @@ low = np.array([40, 50, 50])
 high = np.array([80, 255, 255])
 image_mask = cv2.inRange(hsv, low, high)
 zero = cv2.findNonZero(image_mask)
-temp = [] 
-t = zero[0]
-for x in np.nditer(t):
-    temp.append(str(x))
-t2 = zero[len(zero)-1]
-for x in np.nditer(t2):
-    temp.append(str(x))
 cab = []
-o = str(temp[0]) + ', ' + str(temp[1])
-cab.append(o)
-o = str(temp[2]) + ', ' + str(temp[3])
-cab.append(o)
+cord = []
+while (zero is not None):
+    zero_copy = zero
+    temp = [] 
+    t = zero_copy[0]
+    for x in np.nditer(t):
+        temp.append(str(x))
+    countHeight = 0
+    countWidth = 0
+    for row in zero_copy:
+        if (row[0][0] == int(temp[0])): # does whole height
+            countHeight = countHeight+1
+        if (row[0][1] == int(temp[1])): # does whole width
+            countWidth = countWidth+1
+    o = str(temp[0]) + ', ' + str(temp[1])
+    cord.append(int(temp[0]))
+    cab.append(o)
+    j = 0
+    for row in zero_copy:
+        if ( ((row[0][0] >= int(temp[0]) and row[0][1] >= int(temp[1])) and (row[0][0] <= int(temp[0])+countWidth-1 and row[0][1] <= int(temp[1])+countHeight-1)) and j < len(zero_copy)):
+            img[row[0][1],row[0][0]] = [0,0,0]
+        j = j + 1
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    image_mask = cv2.inRange(hsv, low, high)
+    zero = cv2.findNonZero(image_mask)
+    
 output.cable = cab
-output.cableLength = int(temp[2]) - int(temp[0]) 
-cv2.imshow("Image mask-green", image_mask)
+output.cableLength = cord[1] - cord[0]
+# cv2.imshow("Image mask-green", image_mask)
 
 ## Finding destinations
 low = np.array([100, 50, 50])
@@ -116,10 +131,9 @@ while (zero is not None):
 output.destinations = dest
 # cv2.imshow("Image mask-blue", image_mask)
 
-
 ##JSON 
 s = json.dumps(output.__dict__) 
-with open('inputJson.json', 'w') as outfile:
+with open('json/inputJson.json', 'w') as outfile:
     outfile.write(s)
 outfile.close()
 
